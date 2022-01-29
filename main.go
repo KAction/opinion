c5e6e0ec54a5083febfa4c68b85302f67e65ed00
Dmitry Bogatov <git#v1@kaction.cc>
2022-01-28 23:31:03 -0400

    Mark "PUT" mode as not-implemented

--- main.go
+++ main.go
@@ -4,12 +4,17 @@ import (
 	"flag"
 	"fmt"
 	src "git.sr.ht/~kaction/opinion/src"
+	"os"
 )
 
 func main() {
 	var issueRef src.IssueRef
 	flag.Var(&issueRef, "get", "issue reference in owner/repo#nnn format")
 	flag.Parse()
+	if issueRef.Number == 0 {
+		fmt.Println("Processing data from stdin not implemented")
+		os.Exit(1)
+	}
 
 	fmt.Printf("%v", issueRef)
 }
