f26574bc9214f708a3c23a2036a0f4871957ec66
Dmitry Bogatov <git#v1@kaction.cc>
2022-01-29 21:59:20 -0400

    Refactor response parsing code to use structures

--- main.go
+++ main.go
@@ -65,14 +65,26 @@ type Comment struct {
 	ViewerCanUpdate bool
 }
 
+type PageInfo struct {
+	EndCursor *string
+	HasNextPage bool
+}
+
+type Comments struct {
+	PageInfo PageInfo
+	Nodes []Comment
+}
+
 type Issue struct {
 	Ref src.IssueRef
 	Id string
+	Author Author
 	Title string
 	Body string
 	Closed bool
+	CreatedAt string
 	ViewerCanUpdate bool
-	Comments []Comment
+	Comments Comments
 }
 
 type Args struct {
@@ -100,96 +112,122 @@ func parseArgs() (*Args, error) {
 	return &Args{Ref: ref, Token: token}, nil
 }
 
+func MustMarshal(value interface{}) []byte {
+	bytes, err := json.Marshal(value)
+	if err != nil {
+		format := "Failed to marshal json value: %v %v"
+		panic(fmt.Sprintf(format, value, err))
+	}
+	return bytes
+}
+
+func loadIssue(c *http.Client, ref src.IssueRef, token string) (*Issue, error){
+	const endpoint = "https://api.github.com/graphql"
+	var issue *Issue
+	comments := make([]Comment, 0)
+
+	type Repository struct { Issue *Issue }
+	type Data struct {
+		Data *struct {
+			Repository struct {
+				Issue *Issue
+			}
+		}
+	}
+
+	var cursor *string
+	if c == nil {
+		c = &http.Client{}
+	}
+
+	for {
+		variables := map[string]interface{} {
+			"owner": ref.Owner,
+			"name": ref.Repo,
+			"number": ref.Number,
+			// Can't be bigger: arbitrary limit of GitHub.
+			"batchSize": 100,
+			"cursor": cursor,
+		}
+		payload := map[string]interface{} {
+			"query": fetchQuery,
+			"variables": variables,
+		}
+		payloadBytes := MustMarshal(payload)
+
+		req, err := http.NewRequest("POST", endpoint,
+			bytes.NewReader(payloadBytes))
+		if err != nil {
+			format := "Failed to create request object: %w"
+			return nil, fmt.Errorf(format, err)
+		}
+		req.Header.Add("Authorization",
+			fmt.Sprintf("Bearer %s", token))
+
+		resp, err := c.Do(req)
+		if err != nil {
+			return nil, fmt.Errorf("Request failed: %w", err)
+		}
+		defer resp.Body.Close()
+		bytes, err := io.ReadAll(resp.Body)
+		if err != nil {
+			format := "Failed to read response body: %w"
+			return nil, fmt.Errorf(format, err)
+		}
+		var value Data
+
+		err = json.Unmarshal(bytes, &value)
+		if err != nil {
+			format := "Response is not valid json: %w"
+			return nil, fmt.Errorf(format, err)
+		}
+		issue = value.Data.Repository.Issue
+		comments = append(comments, issue.Comments.Nodes...)
+		if !issue.Comments.PageInfo.HasNextPage {
+			break
+		}
+		cursor = issue.Comments.PageInfo.EndCursor
+	}
+	issue.Ref = ref
+	issue.Comments.Nodes = comments
+	return issue, nil
+}
+
+func printAttrubution(login string, timestamp string) {
+	attribution := fmt.Sprintf(" @%s at %s ", login, timestamp)
+	fmt.Printf("  <!--%s%s--->\n\n",
+		strings.Repeat("-", 69 - len(attribution)),
+		attribution)
+}
+
+func printIssue(i *Issue) {
+	wrapper := textwrap.NewTextWrap().SetWidth(79)
+	status := "(open)"
+	if i.Closed {
+		status = "(closed)"
+	}
+
+	header := fmt.Sprintf("%s/%s#%d%s %s", i.Ref.Owner,
+		i.Ref.Repo, i.Ref.Number, status, i.Title)
+	fmt.Printf("%s\n", header)
+	fmt.Printf("%s\n\n", strings.Repeat("=", len(header)))
+	fmt.Printf("%s\n", i.Body)
+	printAttrubution(i.Author.Login, i.CreatedAt)
+
+	for _, c := range i.Comments.Nodes {
+		fmt.Printf("%s\n", wrapper.Fill(c.Body))
+		printAttrubution(c.Author.Login, c.CreatedAt)
+	}
+}
 
 func main() {
 	args, err := parseArgs()
 	if err != nil {
 		log.Fatal("Failed to parse arguments: %v", err)
 	}
-
-	var cursor *string
-	variables := map[string]interface{} {
-		"owner": args.Ref.Owner,
-		"name": args.Ref.Repo,
-		"number": args.Ref.Number,
-		"batchSize": 20,
-		"cursor": cursor,
-	}
-	payload := map[string]interface{} {
-		"query": fetchQuery,
-		"variables": variables,
-	}
-	payloadBS, err := json.Marshal(payload)
+	issue, err := loadIssue(nil, args.Ref, args.Token)
 	if err != nil {
-		log.Fatal("Failed to marshal json: %v", err)
+		log.Fatal("Failed to load issue: %v", err)
 	}
-
-	req, err := http.NewRequest("POST", "https://api.github.com/graphql",
-		bytes.NewReader(payloadBS))
-	if err != nil {
-		log.Fatal("Failed to create request object: %v", err)
-	}
-		
-	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", args.Token))
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Request failed: %v", err)
-	}
-	defer resp.Body.Close()
-	text, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal("Failed to read response content: %v", err)
-	}
-	var value map[string]interface{}
-
-	err = json.Unmarshal([]byte(text), &value)
-	if err != nil {
-		log.Fatal("Failed to parse response json")
-	}
-	value = value["data"].(map[string]interface{})
-	value = value["repository"].(map[string]interface{})
-	value = value["issue"].(map[string]interface{})
-	createdAt := value["createdAt"].(string)
-	author := value["author"].(map[string]interface{})
-	closed := value["closed"].(bool)
-	title := value["title"].(string)
-	body := value["body"].(string)
-	authorLogin := author["login"].(string)
-	status := "(open)"
-	if closed {
-		status = "(closed)"
-	}
-	// Ugly code that formats issue & comments somehow, without
-	// unmarshalling json into proper datatype hierarchy. Ugly python
-	// style.
-	header := fmt.Sprintf("%s/%s#%d%s %s", args.Ref.Owner,
-		args.Ref.Repo, args.Ref.Number, status, title)
-	fmt.Printf("%s\n", header)
-	fmt.Printf("%s\n\n", strings.Repeat("=", len(header)))
-	fmt.Printf("%s\n", body)
-	attribution := fmt.Sprintf(" @%s at %s ", authorLogin, createdAt)
-
-	fmt.Printf("  <!--%s%s--->\n\n",
-		strings.Repeat("-", 69 - len(attribution)),
-		attribution)
-	value = value["comments"].(map[string]interface{})
-	comments := value["nodes"].([]interface{})
-	wrapper := textwrap.NewTextWrap().SetWidth(79)
-
-	for _, commenti := range comments {
-		comment := commenti.(map[string]interface{})
-		body := comment["body"].(string)
-		author := comment["author"].(map[string]interface{})
-		authorLogin := author["login"].(string)
-		createdAt := comment["createdAt"].(string)
-
-		fmt.Printf("%s\n", wrapper.Fill(body))
-		attribution := fmt.Sprintf(" @%s at %s ", authorLogin,
-			createdAt)
-		fmt.Printf("  <!--%s%s--->\n\n",
-			strings.Repeat("-", 69 - len(attribution)),
-			attribution)
-	}
+	printIssue(issue)
 }
